0dc594ec76c1017854b82e5fcb651082dd2c12c1
Harish Kannarao <harish.gtec@gmail.com>
2015-07-28 17:42:46 +0100

    Created get phone by id endpoint in rest service

--- dev-parent/angularjs-rest-service-war/src/main/java/com/harishkannarao/angularjs/rest/PhoneResource.java
+++ dev-parent/angularjs-rest-service-war/src/main/java/com/harishkannarao/angularjs/rest/PhoneResource.java
@@ -1,15 +1,14 @@
 package com.harishkannarao.angularjs.rest;
 
+import com.harishkannarao.angularjs.model.Phone;
 import com.harishkannarao.angularjs.service.PhoneService;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Optional;
 
 @Path("/phones")
 @Produces({MediaType.APPLICATION_JSON})
@@ -23,5 +22,16 @@ public class PhoneResource {
     @GET
     public Response getAllPhoneSummaries() {
         return Response.ok(phoneService.getAllPhoneSummaries()).build();
+    }
+
+    @GET
+    @Path("/{phoneId}")
+    public Response getPhoneById(@PathParam("phoneId") Long phoneId) {
+        Optional<Phone> phoneOptional = phoneService.getPhoneById(phoneId);
+        if(phoneOptional.isPresent()) {
+            return Response.ok(phoneOptional.get()).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
--- dev-parent/angularjs-rest-service-war/src/main/java/com/harishkannarao/angularjs/service/PhoneService.java
+++ dev-parent/angularjs-rest-service-war/src/main/java/com/harishkannarao/angularjs/service/PhoneService.java
@@ -5,12 +5,15 @@ import com.harishkannarao.angularjs.model.*;
 import javax.ejb.Stateless;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Stateless
 public class PhoneService {
 
     private List<PhoneSummary> phoneSummaries;
-    private List<Phone> phones;
+    private Map<Long, Phone> phones;
 
     public PhoneService() {
         PhoneSummary nexusSPhoneSummary = new PhoneSummaryBuilder().setId(1L)
@@ -54,11 +57,51 @@ public class PhoneService {
                                 .setGps(true)
                                 .setInfrared(false)
                                 .setWifi("802.11 b/g/n"))
+                .setDescription("Nexus S is the next generation of Nexus devices, co-developed by Google and Samsung. The latest Android platform (Gingerbread), paired with a 1 GHz Hummingbird processor and 16GB of memory, makes Nexus S one of the fastest phones on the market. It comes pre-installed with the best of Google apps and enabled with new and popular features like true multi-tasking, Wi-Fi hotspot, Internet Calling, NFC support, and full web browsing. With this device, users will also be the first to receive software upgrades and new Google mobile apps as soon as they become available. For more details, visit http://www.google.com/nexus.")
+                .setDisplayDetails(
+                        DisplayDetailsBuilder.newBuilder()
+                                .setScreenResolution("WVGA (800 x 480)")
+                                .setScreenSize("4.0 inches")
+                                .setTouchScreen(true))
+                .setHardwareDetails(
+                        HardwareDetailsBuilder.newBuilder()
+                                .setAccelerometer(true)
+                                .setAudioJack("3.5mm")
+                                .setCpu("1GHz Cortex A8 (Hummingbird) processor")
+                                .setFmRadio(false)
+                                .setPhysicalKeyboard(false)
+                                .setUsb("USB 2.0")
+                )
+                .setId(1L)
+                .setImages(
+                        Arrays.asList(
+                                "/restapi/img/phones/nexus-s.0.jpg",
+                                "/restapi/img/phones/nexus-s.1.jpg",
+                                "/restapi/img/phones/nexus-s.2.jpg",
+                                "/restapi/img/phones/nexus-s.3.jpg"
+                        )
+                )
+                .setName("Nexus S")
+                .setSizeAndWeightDetails(
+                        SizeAndWeightDetailsBuilder.newBuilder()
+                                .setDimensions(Arrays.asList("63.0 mm (w)", "123.9 mm (h)", "10.88 mm (d)"))
+                                .setWeight("129.0 grams")
+                )
+                .setStorageDetails(
+                        StorageDetailsBuilder.newBuilder()
+                                .setFlash("16384MB")
+                                .setRam("512MB")
+                )
                 .build();
 
+        phones = Arrays.asList(nexusSPhone).stream().collect(Collectors.toMap(Phone::getId, (Phone phone) -> phone));
     }
 
     public List<PhoneSummary> getAllPhoneSummaries() {
         return phoneSummaries;
+    }
+
+    public Optional<Phone> getPhoneById(Long id) {
+        return Optional.ofNullable(phones.get(id));
     }
 }
