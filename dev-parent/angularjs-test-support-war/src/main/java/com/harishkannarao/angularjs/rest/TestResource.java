36beff528a64898c80e0c836a74e52bf331e9897
Harish Kannarao <harish.gtec@gmail.com>
2015-08-19 17:44:30 +0100

    Created angularjs test support war Added to the deployment

---
+++ dev-parent/angularjs-test-support-war/src/main/java/com/harishkannarao/angularjs/rest/TestResource.java
@@ -0,0 +1,19 @@
+package com.harishkannarao.angularjs.rest;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/test")
+@Stateless
+public class TestResource {
+
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    public Response getMessage() {
+        return Response.ok("Success !!!").build();
+    }
+}
