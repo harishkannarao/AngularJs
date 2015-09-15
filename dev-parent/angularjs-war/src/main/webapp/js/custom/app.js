e33ab71e7f485a8cd56231d71bac5a1d07c00693
Harish Kannarao <harish.gtec@gmail.com>
2015-09-15 18:00:58 +0100

    Technical improvement: - Reintroducing scroll to function

--- dev-parent/angularjs-war/src/main/webapp/js/custom/app.js
+++ dev-parent/angularjs-war/src/main/webapp/js/custom/app.js
@@ -131,3 +131,11 @@ phonecatApp.config(['$httpProvider', function ($httpProvider) {
 }]);
 
 
+phonecatApp.run(['$rootScope', '$location', '$anchorScroll', '$routeParams', function($rootScope, $location, $anchorScroll, $routeParams) {
+  //when the route is changed scroll to the proper element
+  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+    $location.hash($routeParams.scrollTo);
+    $location.replace();
+  });
+}]);
+
